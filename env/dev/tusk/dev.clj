3c8c0a70bef5852c0c5bebba6f47b4a248c15db6
Faris Nasution <funyako.funyao156@gmail.com>
2018-05-27 20:32:41 +0700

    Add `toggle-assertion!`

--- env/dev/tusk/dev.clj
+++ env/dev/tusk/dev.clj
@@ -1,16 +1,19 @@
 (ns tusk.dev
   (:require
    [clojure.repl :refer :all]
+   [clojure.spec.alpha :as s]
    [com.stuartsierra.component.repl :as cr]
    [tusk.system :as st]))
+
+(defn toggle-assertion!
+  []
+  (if (s/check-asserts?)
+    (s/check-asserts false)
+    (s/check-asserts true)))
 
 (defn create-system!
   []
   (cr/set-init (constantly (st/create-system-map {:profile :dev}))))
-
-(defn delete-system!
-  []
-  (cr/set-init (constantly nil)))
 
 (defn start!
   []
